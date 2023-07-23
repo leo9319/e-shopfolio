f49cff76adc43adeb4743e254ad5326fa8e991a1
Leonard <leo_9319@yahoo.com>
2023-07-23 16:59:24 -0400

    add categories information

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Category;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class CategoryController extends Controller
+{
+    public function index() {
+        $categories = Category::paginate(10);
+
+        return Inertia::render('Categories', [
+            'categories' => $categories,
+        ]);
+    }
+}
